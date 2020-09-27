594d3a1bcb751cead6952c8c5551ca1d9c953d1f
Cuyler Stuwe <salembeats@gmail.com>
2020-09-27 15:39:56 -0700

    de-stringify manifest.js

--- src/manifest.js
+++ src/manifest.js
@@ -5,27 +5,27 @@ const packageJson = JSON.parse(packageJsonText);
 
 const manifest = {
 
-    "manifest_version": 2,
-    "name": packageJson.longName || "package.json is missing a name",
-    "description": packageJson.description || "package.json is missing a description",
-    "version": packageJson.version,
+    manifest_version: 2,
+    name: packageJson.longName || "package.json is missing a name",
+    description: packageJson.description || "package.json is missing a description",
+    version: packageJson.version,
 
-    "browser_action": {
-        "default_icon": {
+    browser_action: {
+        default_icon: {
             "16": "icons/icon16.png",
             "32": "icons/icon32.png",
             "48": "icons/icon48.png",
             "64": "icons/icon64.png"
         },
-        "default_title": packageJson.longName,
-        "default_popup": "popup.html"
+        default_title: packageJson.longName,
+        default_popup: "popup.html"
     },
 
-    "background": {
-        "page": "background.html"
+    background: {
+        page: "background.html"
     },
 
-    "content_scripts": [
+    content_scripts: [
         {
             matches: ["<all_urls>"], // TODO: Make this more specific, rather than the template default of matching everything.
             js: ["content.js"]
