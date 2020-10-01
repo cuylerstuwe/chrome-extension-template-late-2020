0b28f50b22cc51676f424ed3defdc4c03b0054e8
Cuyler Stuwe <salembeats@gmail.com>
2020-10-01 15:59:32 -0700

    programmatically generate icons config object in manifest.js

--- src/manifest.js
+++ src/manifest.js
@@ -3,6 +3,18 @@ const fs = require("fs");
 const packageJsonText = fs.readFileSync("../package.json");
 const packageJson = JSON.parse(packageJsonText);
 
+const iconSizes = [16, 32, 48, 64];
+
+const iconDirectory = "icons";
+const iconFilenamePrefix = "icon";
+const iconFiletypeSuffix = "png";
+
+const iconsObj = Object.fromEntries(
+    iconSizes.map(iconSize => (
+        [iconSize, `${iconDirectory}/${iconFilenamePrefix}${iconSize}.${iconFiletypeSuffix}`]
+    ))
+);
+
 const manifest = {
 
     manifest_version: 2,
@@ -10,20 +22,10 @@ const manifest = {
     description: packageJson.description || "package.json is missing a description",
     version: packageJson.version,
 
-    icons: {
-        "16": "icons/icon16.png",
-        "32": "icons/icon32.png",
-        "48": "icons/icon48.png",
-        "64": "icons/icon64.png"
-    },
+    icons: iconsObj,
 
     browser_action: {
-        default_icon: {
-            "16": "icons/icon16.png",
-            "32": "icons/icon32.png",
-            "48": "icons/icon48.png",
-            "64": "icons/icon64.png"
-        },
+        default_icon: iconsObj,
         default_title: packageJson.longName,
         default_popup: "popup.html"
     },
