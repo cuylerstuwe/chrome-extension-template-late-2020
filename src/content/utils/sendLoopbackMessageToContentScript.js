3ecb9089241770e6fdefa079235f9f7d3e915644
Cuyler Stuwe <salembeats@gmail.com>
2020-10-01 15:46:45 -0700

    move sendLoopbackMessageToContentScript to correct /utils subfolder

--- src/content/utils/sendLoopbackMessageToContentScript.js
+++ src/content/utils/sendLoopbackMessageToContentScript.js
@@ -1,4 +1,4 @@
-import config from "../config";
+import config from "../../config";
 
 async function sendLoopbackMessageToContentScript(message) {
     const returnValue = await window[config.CONTENT_SCRIPT_LISTENER_LOOPBACK_NAME](message);
