45acc67cb05749daffce3a42a85c30b8f3342668
Cuyler Stuwe <salembeats@gmail.com>
2020-10-01 14:08:15 -0700

    introduce state and message pump generator

---
+++ src/content/modules/noop.js
@@ -0,0 +1,22 @@
+import messageTypes from "../messageTypes";
+const messageType = messageTypes.NOOP;
+
+export default ({
+
+    messageListener: ({store}) => async argsObj => {
+
+        store.dispatch({
+            type: messageType,
+        });
+
+        return store.getState();
+
+    },
+
+    reducer: ({state, action}) => ({
+        ...state
+    }),
+
+    messageType
+
+});
