d68759ca4a023c4b00255cf2b7588d4f9a941536
Cuyler Stuwe <salembeats@gmail.com>
2021-06-28 19:17:21 -0700

    add onBackgroundReady to encourage not writing code directly in background.js, using it instead as boilerplate

--- src/background/background.js
+++ src/background/background.js
@@ -1,6 +1,7 @@
 import "../utils/startedLog";
 import globalBackgroundState from "./globalBackgroundState";
 import config from "../config";
+import onBackgroundReady from "./onBackgroundReady";
 
 const _noop = async () => {};
 const _switch = async (pathName, branchesObj, argsObj) => await ((branchesObj[pathName] || branchesObj.default || _noop)(argsObj));
@@ -24,3 +25,5 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 });
 
 console.log("Runtime environment:", { BUILD_ENV: process.env.BUILD_ENV, NODE_ENV: process.env.NODE_ENV });
+
+onBackgroundReady();
---
+++ src/background/onBackgroundReady.js
@@ -0,0 +1,4 @@
+export default async function onBackgroundReady() {
+    // TODO: Write whatever code should execute once the background script is ready.
+    //  This function should run once on init, after the extension's core message pump and background modules are prepared to send/receive.
+}
